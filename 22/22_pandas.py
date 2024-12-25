c369d1885337ff804d7f7dfa96e0efeb92b24246
i-borisov <165560297+i-borisov@users.noreply.github.com>
2024-12-25 22:28:39 +0300

    Update 22_pandas.py

--- 22/22_pandas.py
+++ 22/22_pandas.py
@@ -1,4 +1,4 @@
-# Задача 22-4 (Pandas)
+# Задача 22-4
 
 import pandas as pd
 
@@ -9,3 +9,23 @@ data = {'product_id': [101, 102, 101, 103, 102, 101, 103],
 df = pd.DataFrame(data)
 
 df_store = df.groupby(['store_id', 'product_id']).agg({'sales': ['sum', 'mean', 'max']})
+
+
+# Задача 22-5
+
+import pandas as pd
+
+data = {'product_id': [101, 102, 101, 103, 102, 101, 103],
+        'store_id': [1, 1, 2, 1, 2, 2, 1],
+        'sales': [5, 3, 9, 7, 2, 4, 8]}
+
+df = pd.DataFrame(data)
+
+# функция для проверки чётности числа
+def is_even(number):
+  if number % 2 == 0:
+    return 'Чётное'
+  else:
+    return 'Нечётное'
+# Применяем функцию к каждому полю столбца 'store_id' и сохраняем получившиеся значения в столбец 'even'
+df['even'] = df['store_id'].map(is_even)
