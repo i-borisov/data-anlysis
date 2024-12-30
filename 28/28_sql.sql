748fd43d89378fe3f3623d15778462d31e6726f1
i-borisov <165560297+i-borisov@users.noreply.github.com>
2024-12-30 21:29:55 +0300

    Update 28_sql.sql

--- 28/28_sql.sql
+++ 28/28_sql.sql
@@ -1,11 +1,75 @@
--- ЗАДАЧА 28-1
+-- ЗАДАЧА 29-1
 
 SELECT
-    p.name,
+    program_id,
     COUNT(order_id) AS orders_count
-FROM orders AS o
-INNER JOIN programs AS p ON o.program_id = p.id
-WHERE DATE_TRUNC('month', order_date) = DATE_TRUNC('month', CURRENT_TIME)
-GROUP BY p.name
+FROM orders
+WHERE DATE_TRUNC('month', order_date) = DATE_TRUNC('month', CURRENT_DATE)
+GROUP BY program_id
 ORDER BY orders_count DESC
 LIMIT 5;
+
+
+-- ЗАДАЧА 29-2
+
+SELECT
+    program_id,
+    SUM(order_sum) AS amount
+FROM orders
+WHERE state = 'success'
+GROUP BY program_id;
+
+
+-- ЗАДАЧА 29-3
+
+WITH analytics AS (
+    SELECT DISTINCT id
+    FROM programs
+    WHERE direction = 'Аналитика'
+)
+
+SELECT program_id
+FROM orders AS o
+INNER JOIN analytics AS a ON o.program_id = a.id
+WHERE (order_date BETWEEN '2021-11-01' AND '2021-12-31') AND (state = 'success')
+GROUP BY program_id
+HAVING SUM(order_sum) >= 1000000;
+
+
+-- ЗАДАЧА 29-4
+
+SELECT DISTINCT
+    user_id,
+    FIRST_VALUE(program_id) OVER (PARTITION BY user_id ORDER BY buy_date) AS first_program
+FROM orders
+WHERE sate = 'success';
+
+
+-- ЗАДАЧА 29-5
+
+SELECT DISTINCT
+    user_id,
+    LEAD(program_id, 3) OVER (PARTITION BY user_id ORDER BY buy_date) AS third_program
+FROM orders
+WHERE sate = 'success';
+
+
+-- ЗАДАЧА 29-6
+
+WITH tmp AS (
+    SELECT DISTINCT
+        user_id,
+        order_id,
+        LEAD(program_id, 2) OVER (PARTITION BY user_id ORDER BY buy_date) AS third,
+        FIRST_VALUE(program_id) OVER (PARTITION BY user_id ORDER BY buy_date) AS first
+    FROM orders
+    WHERE sate = 'success'
+)
+
+SELECT
+    user_id,
+    CASE
+    WHEN third IS NULL THEN first
+    ELSE third
+    END AS program_id
+FROM tmp;
