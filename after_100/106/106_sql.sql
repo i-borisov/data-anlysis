f2cdfe88d0b9a1cf5ee75bee24d7aa39096b9f92
i-borisov <165560297+i-borisov@users.noreply.github.com>
2025-03-20 22:17:48 +0300

    Create 106_sql.sql

---
+++ after_100/106/106_sql.sql
@@ -0,0 +1,16 @@
+-- ЗАДАЧА 2
+
+WITH tmp AS (
+    SELECT
+        DATE_TRUNC('month', order_date) AS month,
+        COUNT(DISTINCT client_id) AS clients_month
+    FROM Orders
+    GROUP BY DATE_TRUNC('month', order_date)
+)
+
+SELECT
+    month,
+    clients_month,
+    LAG(clients_month) OVER (ORDER BY month) AS clients_prev_month
+FROM tmp
+ORDER BY month DESC;
