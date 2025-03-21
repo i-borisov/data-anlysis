db820dacb90e571f5faeb74b5fcff916e4c060a3
i-borisov <165560297+i-borisov@users.noreply.github.com>
2025-03-21 15:05:55 +0300

    Update 106_sql.sql

--- after_100/106/106_sql.sql
+++ after_100/106/106_sql.sql
@@ -20,3 +20,23 @@ SELECT
     LAG(clients_month) OVER (ORDER BY month) AS clients_prev_month
 FROM tmp
 ORDER BY month DESC;
+
+-- ЗАДАЧА 3
+
+WITH uniq_client AS (
+    SELECT DISTINCT
+        client_id,
+        client_name,
+        partition_date AS actual_number,
+        LEAD(partition_date, 1, '2100-01-01') OVER (PARTITION BY client_id ORDER BY partition_date) AS next_number
+    FROM clients_phones_ivb
+ORDER BY client_id)
+
+SELECT DISTINCT
+    DATE_TRUNC('month', o.order_date) AS MONTH,
+    o.client_id,
+    u.client_name,
+    SUM(order_price) OVER (PARTITION BY DATE_TRUNC('month', o.order_date), o.client_id, u.client_name)
+FROM orders_ivb AS o
+INNER JOIN uniq_client AS u ON o.client_id = u.client_id AND (o.order_date >= u.actual_number AND o.order_date < u.next_number)
+ORDER BY o.client_id, month;
