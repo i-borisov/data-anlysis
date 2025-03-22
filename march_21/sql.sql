0fa0e7ede813a597419bc07b36e7693193a79ff2
i-borisov <165560297+i-borisov@users.noreply.github.com>
2025-03-22 22:12:57 +0300

    Create sql.sql

---
+++ march_21/sql.sql
@@ -0,0 +1,36 @@
+-- ЗАДАЧА 1-1
+
+INNER JOIN t1.a = t2.a -- 2
+LEFT JOIN t1.a = t2.a -- 3
+FULL OUTER JOIN t1.a = t2.a -- 5
+
+
+-- ЗАДАЧА 1-2
+
+-- Запрос 1
+1-1
+1-1
+2-2
+2-2
+2-2
+2-2
+3-3
+4-NULL
+NULL-NULL
+
+-- Запрос 2
+1-1
+1-1
+2-2
+2-2
+2-2
+2-2
+3-3
+
+
+-- ЗАДАЧА 1-3
+
+INNER JOIN -- 0/12
+LEFT JOIN -- 3/12
+FULL JOIN -- 4/12
+CROSS JOIN -- 12/12
