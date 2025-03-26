4887992b7fa0006628bffe13bfa36d8c84578f14
i-borisov <165560297+i-borisov@users.noreply.github.com>
2025-03-26 19:16:15 +0300

    Добавлены задачи 7 - 15

--- march_21/pandas.py
+++ march_21/pandas.py
@@ -28,3 +28,44 @@ print(avg_prices)
 print(avg_prices.sort_values('avg', ascending=False).head(5))
 
 
+# 7. Найти суммарную выручку по каждому товару.
+df['amount'] = df['price'] * df['quantity']
+df_amount = df.groupby('product').amount.sum()
+print(df_amount)
+
+
+# 8. Найти самый прибыльный товар.
+df_amount.idxmax()
+
+
+# 9. Определить, сколько заказов сделал каждый клиент.
+orders_count = df.groupby('customer_name').order_id.count().sort_values(ascending=False)
+orders_count
+
+
+# 10. Найти клиента, совершившего больше всего заказов.
+orders_count.idxmax()
+
+
+# 11. Определить, какой клиент потратил больше всего денег.
+df['amount'] = df['price'] * df['quantity']
+best_client = df.groupby('customer_name').amount.sum().idxmax()
+print(best_client)
+
+
+# 12. Определить средний чек заказа.
+aov = df['amount'].mean()
+print(aov)
+
+
+# 13. Найти общее количество проданных товаров.
+print(df.quantity.sum())
+
+
+# 14. Определить товар с наибольшим общим количеством продаж.
+top_product = df.groupby('product').quantity.sum().idxmax()
+print(top_product)
+
+
+# 15. Найти средний рейтинг товаров.
+print(df.rating.mean())
