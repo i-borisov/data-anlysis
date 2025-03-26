60f95714372938f6c3e0bfccadaae63c6be19303
i-borisov <165560297+i-borisov@users.noreply.github.com>
2025-03-26 16:56:10 +0300

    Добавлены задачи с 5 по 8

--- march_21/python.py
+++ march_21/python.py
@@ -26,7 +26,7 @@ def count_char(s, char):
     for symbol in s:
       if symbol == char:
         count += 1
-    
+
     return count
 
 print(count_char("hellllllo", "l"))
@@ -44,3 +44,63 @@ def remove_vowels(s):
     return ''.join(result)
 
 print(remove_vowels("hello"))  # "hll"
+
+
+# 5. Найти самое длинное слово в строке
+def longest_word(s):
+  words = s.split()
+  max_length = 0
+  result = []
+
+  for word in words:
+    if len(word) >= max_length:
+      max_length = len(word)
+
+  for word in words:
+    if len(word) == max_length:
+      result.append(word)
+
+  return result
+
+print(longest_word("hello world this is a test"))  # ["hello", "world"]
+
+
+# 6. Заменить все пробелы в строке на подчеркивания
+def replace_spaces(s):
+    return s.replace(' ', '_')
+
+print(replace_spaces("hello world"))  # "hello_world"
+
+def replace_spaces_v2(s):
+  new = []
+
+  for e in s:
+    if e != ' ':
+      new.append(e)
+    else:
+      new.append('_')
+
+  return ''.join(new)
+
+print(replace_spaces_v2("hello world"))  # "hello_world"
+
+
+# 7. Удалить все цифры из строки
+def remove_digits(s):
+  result = []
+  digits = '1234567890'
+
+  for element in s:
+    if element not in digits:
+      result.append(element)
+
+  return ''.join(result)
+
+print(remove_digits("hello123"))  # "hello"
+
+
+# 8. Вернуть первые n символов строки
+def first_n_chars(s, n):
+    return s[:n]
+
+print(first_n_chars("hello", 3))  # "hel"
