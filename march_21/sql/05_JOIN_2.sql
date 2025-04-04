acd946dc7403f9c918f2d1b332d9310685a5b07e
i-borisov <165560297+i-borisov@users.noreply.github.com>
2025-04-04 16:43:19 +0300

    Решены все задачи.

--- march_21/sql/05_JOIN_2.sql
+++ march_21/sql/05_JOIN_2.sql
@@ -8,3 +8,26 @@ INNER JOIN logins AS l2 ON l1.user_id = l2.user_id
 AND
 DATE_TRUNC('month', l1.date) - INTERVAL '1 month' = DATE_TRUNC('month', l2.date)
 GROUP BY DATE_TRUNC('month', l1.date);
+
+
+-- ЗАДАЧА 5-2
+
+SELECT
+    s.prod_cat,
+    SUM(s.price * s.cnt) AS total_amt
+FROM sale AS s
+INNER JOIN product AS p ON s.prod_nm = p.prod_nm  AND s.sale_dt BETWEEN p.dt_from AND p.dt_to
+GROUP BY s.prod_cat
+ORDER BY prod_cat;
+
+
+-- ЗАДАЧА 5-3 (Дубль задачи 5-2)
+
+
+-- ЗАДАЧА 5-4
+
+SELECT
+    a1.color,
+    a2.color
+FROM a AS a1
+INNER JOIN a AS a2 ON a1.clolr < a2.color;
