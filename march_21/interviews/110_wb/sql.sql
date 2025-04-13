7cae06517c4c4c86d097309b309a48ba34e4111b
i-borisov <165560297+i-borisov@users.noreply.github.com>
2025-04-13 18:45:04 +0300

    Update sql.sql

--- march_21/interviews/110_wb/sql.sql
+++ march_21/interviews/110_wb/sql.sql
@@ -56,3 +56,42 @@ SELECT DISTINCT
     "Метод Платежа" AS payment_method,
     ROUND((100.0 * SUM(Оборот) OVER (PARTITION BY Дата, "Метод платежа") / SUM(Оборот) OVER (PARTITION BY Дата)), 2) AS rate
 FROM tab;
+
+
+-- ЗАДАЧА 4
+
+--вариант 1
+WITH top AS (
+SELECT employee_id
+FROM tab
+WHERE manager_id = 1 AND manager_id <> employee_id
+), middle AS (
+    SELECT tab.employee_id
+    FROM top
+    INNER JOIN tab ON top.employee_id = tab.manager_id
+), low AS (
+    SELECT tab.employee_id
+    FROM middle
+    INNER JOIN tab ON middle.employee_id = tab.manager_id
+)
+
+SELECT employee_id
+FROM top
+
+UNION
+
+SELECT employee_id
+FROM middle    
+
+UNION
+
+SELECT employee_id
+FROM low;
+
+--вариант 2
+
+SELECT t1.employee_id
+FROM tab AS t1
+LEFT JOIN tab AS t2 ON t1.employee_id = t2.manager_id
+LEFT JOIN tab AS t3 ON t2.employee_id = t3.manager_id
+WHERE t1.manager_id = 1 AND t1.employee_id <> t1.manager_id;
